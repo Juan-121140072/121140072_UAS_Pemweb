b12ea1adfa696d4e6868dd856b1cb50fc0320101
Juan Verrel Tanuwijaya <126883141+Juan-121140072@users.noreply.github.com>
2023-12-17 01:04:42 +0700

    Add files via upload

--- db.sql
+++ db.sql
@@ -1,23 +1,83 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Dec 16, 2023 at 05:59 PM
+-- Server version: 10.5.20-MariaDB
+-- PHP Version: 7.3.33
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-CREATE TABLE `user` (
-  `id_user` bigint(10) NOT NULL,
-  `nama` varchar(50) NOT NULL,
-  `prodi` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-ALTER TABLE `mahasiswa`
-  ADD PRIMARY KEY (`nim`);
-COMMIT;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `id21680724_uas_pemweb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `makanan`
+--
 
 CREATE TABLE `makanan` (
-  `nim` bigint(10) NOT NULL,
+  `id` varchar(50) NOT NULL,
   `nama` varchar(50) NOT NULL,
-  `prodi` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `kehalalan` varchar(50) NOT NULL,
+  `jenis` varchar(50) NOT NULL,
+  `ciri` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-ALTER TABLE `mahasiswa`
-  ADD PRIMARY KEY (`nim`);
+--
+-- Dumping data for table `makanan`
+--
+
+INSERT INTO `makanan` (`id`, `nama`, `kehalalan`, `jenis`, `ciri`) VALUES
+('1', 'Pecel Lele', 'Halal', 'Main Course', 'Asin, Pedas, Kering'),
+('2', 'Es Cendol', 'Halal', 'Desert', 'Manis');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`username`, `password`) VALUES
+('admin', '123456');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `makanan`
+--
+ALTER TABLE `makanan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`username`);
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
